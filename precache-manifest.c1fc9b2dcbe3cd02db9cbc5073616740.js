19ea452724269289020c60d3e39db6c93777abac
mpaulweeks <mpaulweeks@gmail.com>
2020-05-02 18:51:38 -0400

    Initial commit

---
+++ precache-manifest.c1fc9b2dcbe3cd02db9cbc5073616740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b64a66e4e416614bb58eb5ddc71e174",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c5351b6b18971adcd6b",
+    "url": "./static/css/main.8b8944d7.chunk.css"
+  },
+  {
+    "revision": "de2067206720501208bc",
+    "url": "./static/js/2.7b5d5849.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "./static/js/2.7b5d5849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5351b6b18971adcd6b",
+    "url": "./static/js/main.d9c7552d.chunk.js"
+  },
+  {
+    "revision": "fa85b1b61ff7635c6341",
+    "url": "./static/js/runtime-main.3469be4d.js"
+  }
+]);
