fed8b3842946542b03d1624dfd2cf0c1becdab92
mpaulweeks <mpaulweeks@gmail.com>
2020-05-09 22:42:50 -0400

    display players when game not started

--- src/fe/ViewBar.tsx
+++ src/fe/ViewBar.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { AllRoles } from '../core/role';
 import { GameData, RoleType, UserState } from '../core/types';
 import { StyledBox } from './shared';
-import { getCurrentPlayers } from '../core/utils';
+import { getCurrentPlayers, sortObjVals } from '../core/utils';
 
 export const SecretBox = styled(StyledBox)`
   background-color: black;
@@ -47,6 +47,8 @@ export class ViewBar extends React.Component<Props, State> {
     const nominations = data.nominations.roster.length > 0 ? data.nominations.roster.map(pid => data.players[pid].name).join(', ') : '(nobody)';
     const playerIds = data.turn ? data.turn.order : getCurrentPlayers(data).map(p => p.pid);
 
+    const allPlayers = sortObjVals(data.players, p => p.name);
+
     return (
       <div>
         <SecretBox onClick={() => this.setState({ collapseSecrets: !collapseSecrets, })}>
@@ -76,24 +78,32 @@ export class ViewBar extends React.Component<Props, State> {
             )}
         </SecretBox>
         <RoleBox>
-          <div>
-            turn order: {playerIds.map((pid, index, array) => {
-            const pdata = data.players[pid];
-            const name = (pdata ? pdata.name : '???') + (index < array.length - 1 ? ',' : '');
-            const style: CSSProperties = {
-              color: pid === data.host ? 'purple' : 'black',
-              textDecoration: (data.turn && pid === data.turn.current) ? 'underline' : 'none',
-            };
-            return (
-              <span key={pid} style={style}>
-                {name}
-              </span>
-            );
-          })}
-          </div>
-          <div>
-            nomination: {nominations}
-          </div>
+          {data.turn ? (
+            <div>
+              <div>
+                turn order: {playerIds.map((pid, index, array) => {
+                const pdata = data.players[pid];
+                const name = (pdata ? pdata.name : '???') + (index < array.length - 1 ? ',' : '');
+                const style: CSSProperties = {
+                  color: pid === data.host ? 'purple' : 'black',
+                  textDecoration: (data.turn && pid === data.turn.current) ? 'underline' : 'none',
+                };
+                return (
+                  <span key={pid} style={style}>
+                    {name}
+                  </span>
+                );
+              })}
+              </div>
+              <div>
+                nomination: {nominations}
+              </div>
+            </div>
+          ) : (
+              <div>
+                players: {allPlayers.map(p => p.name).join(', ')}
+              </div>
+            )}
         </RoleBox>
       </div>
     );
