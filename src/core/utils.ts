d8777d010fecb727df135af8cf21343f7eb0f178
mpaulweeks <mpaulweeks@gmail.com>
2020-05-11 20:26:59 -0400

    1.4.0

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,6 +1,6 @@
 import { GameData, MissionResultType } from "./types";
 
-export const APP_VERSION = '1.3.2';
+export const APP_VERSION = '1.4.0';
 
 export const isDev = window.location.href.includes('localhost');
 export const isDebug = window.location.href.includes('?d');
